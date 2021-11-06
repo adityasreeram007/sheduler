64e423bd89f2254c27aab2a3b6f09acb33ceea20
adityasreeram007 <adityasreeram99@gmail.com>
2021-11-06 16:31:47 +0530

    start

---
+++ sheduler-ui/src/App.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import { Navbar,Nav,NavDropdown,Container } from 'react-bootstrap'
+import './App.css'
+class App extends Component{
+  render(){
+      return (
+
+        <main>
+        <nav class="navbar navbar-expand-sm bg-dark">
+  <ul class="navbar-nav">
+    <li class="nav-item">
+     Sheduler
+    </li>
+    <li class="nav-item">
+     Dashboard
+    </li>
+    <li class="nav-item">
+     Connect
+    </li>
+    <li class="nav-item">
+     profile
+    </li>
+  </ul>
+</nav>
+        </main>
+
+      )
+
+
+  }
+}
+export default App
