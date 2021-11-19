b5a8b5d3644391b5129718073c1fd8d1f89cf5ab
s0n03hp <sr.sankar.narayanan@walmart.com>
2021-11-19 06:27:53 +0530

    start

---
+++ sheduler-ui/src/components/Card.jsx
@@ -0,0 +1,19 @@
+import { Component } from "react";
+
+class Card extends Component{
+    render(){
+        return (
+
+
+            <div class="card">
+                <div>Name of the Event</div>
+               <div>25th November 2021</div> 
+               <div>04:30PM</div>
+               <div>Notes</div>
+            </div>
+        )
+    }
+}
+
+
+export default Card
--- sheduler-ui/src/components/DashBoard.jsx
+++ sheduler-ui/src/components/DashBoard.jsx
@@ -1,24 +1,77 @@
 import { Component } from "react";
 
+import Calendar from 'react-awesome-calendar';
+import Modal from 'react-modal';
+import { Button } from 'react-bootstrap'
+import Card from './Card'
 class DashBoard extends Component{
-
+    state={
+      isOpen:false
+    }
 
     render(){
         return (
-<div class="container">
-  <div class="row">
-    <div class="col-sm">
-      One of three columns
-      <h1>DashBoard</h1>
-    </div>
-    <div class="col-sm">
-      One of three columns
-    </div>
-    <div class="col-sm">
-      One of three 
-    </div>
-  </div>
-</div>
+            <div class="box">
+              <div class="row">
+               <div class="messages">
+                    You can connect with other available platforms in connect tab
+                  </div></div>
+                  <div class="row">
+                  <div class="col-sm-9 head">
+                    <div class="row">
+                    <div class="col-sm-10">
+                  <h3>Events Dashboard</h3>
+                  </div>
+                  <div class="col-sm-2"><Button variant="light" class="btn" onClick={()=>{
+                    this.setState({
+                      isOpen:true
+                    })
+                  }}>Schedule</Button></div></div>
+                </div>
+                <div class="col-sm-3 log"><h3>Logs</h3></div>
+              
+                
+                </div>
+
+              <div class="row">
+                
+                <div class="col-sm-9 dashboard">
+                  
+                 
+                  <Calendar onClickEvent={(data)=>{
+                    console.log(data)
+                  }} 
+                  onClickTimeLine={(data)=>{
+                    console.log(data)
+                  }}/>
+                </div>
+                <div class="col-sm-3 sideboard">
+                  <div class="row ">
+                 <Card/>
+                  
+                    </div>
+
+                </div>
+               
+              </div>
+              <Modal isOpen={this.state.isOpen} style={{content:{
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    height:'50%',
+    width:'50%',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  }}} >
+    <Button onClick={()=>{
+      this.setState({
+        isOpen:false
+      })
+    }}>Close</Button>
+
+              </Modal>
+            </div>
             
 
         
