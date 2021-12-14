ba1e7efc405f86dde1e74f6cdca58b494792f99c
adityasreeram007 <adityasreeram99@gmail.com>
2021-12-14 13:32:36 +0530

    chat

--- sheduler-ui/src/components/ChatArea.jsx
+++ sheduler-ui/src/components/ChatArea.jsx
@@ -1,13 +1,27 @@
 import { Component } from "react";
-
+import ReactSearchBox from "react-search-box";
 class ChatArea extends Component{
     render(){
         return(
+            <>
 <div className="row">
-    div.
-    Name
+    <div className="headname">
+        Arnold
+    </div>
+</div>
+<div className="messagearea">
+
+</div>
+<div className="sendarea">
+    <div className="row">
+        <div className="col-sm-10">
+<ReactSearchBox placeholder="start typing" /></div>
+<div className="col-sm-2">
+<button class="btn btn-success">send</button></div>
 </div>
 
+</div>
+</>
         )
     }
 }
